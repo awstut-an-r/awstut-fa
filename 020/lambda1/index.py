5324d6e5481feb202dd5ba0fc174153028819528
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-02-01 20:30:15 +0900

    Update index.py

--- 020/lambda1/index.py
+++ 020/lambda1/index.py
@@ -5,11 +5,6 @@ import mymodule
 
 def lambda_handler(event, context):
   ssm_parameter_name = os.environ['ssm_parameter_name']
-  #region_name = os.environ['region_name']
-  #
-  #ssm = boto3.client('ssm', region_name=region_name)
-  #ssm_parameter = ssm.get_parameter(
-  #  Name=ssm_parameter_name)['Parameter']['Value']
   ssm_parameter = mymodule.get_ssm_parameter(ssm_parameter_name)
 
   return {
