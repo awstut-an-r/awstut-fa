f265e27e8bf621a45043301378866d5e61fc8196
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-04-02 22:24:01 +0900

    Update browser.js

--- 036/browser.js
+++ 036/browser.js
@@ -1,13 +1,3 @@
-/*
-import {
-  CognitoIdentityClient
-} from "@aws-sdk/client-cognito-identity";
-
-import {
-  fromCognitoIdentity
-} from "@aws-sdk/credential-provider-cognito-identity";
-*/
-
 import {
   CognitoIdentityProviderClient,
   GetUserCommand
@@ -21,21 +11,18 @@ const getPayloadFromJWT = (token) => {
 
 const REGION = "ap-northeast-1";
 const USER_POOL_ID = "ap-northeast-1_nd984pe0j";
-//const USER_NAME = "awstut";
 
 const params = new URLSearchParams(location.hash.slice(1));
 const accessToken = params.get("access_token");
 const idToken = params.get("id_token");
 
 const showAccessToken = () => {
-  //console.log(accessToken);
   document.getElementById("access-token").innerText = JSON.stringify(
     JSON.parse(atob(getPayloadFromJWT(accessToken))), null , 2);
 };
 
 const showIdToken = () => {
   if (idToken !== null) {
-    //console.log(idToken);
     document.getElementById("id-token").innerText = JSON.stringify(
       JSON.parse(atob(getPayloadFromJWT(idToken))), null , 2);
   } else {
@@ -54,10 +41,8 @@ const showUser = async () => {
         AccessToken: accessToken
       })
     );
-    //console.log(response);
     document.getElementById("get-user").innerText = JSON.stringify(response, null, 2);
   } catch (err) {
-    //console.log(err);
     document.getElementById("get-user").innerText = JSON.stringify(err, null, 2);
   }
 }
