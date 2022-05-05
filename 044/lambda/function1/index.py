bdff2ead1b60f67cf3bcf4551488c14b8d160c65
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-05-05 12:58:57 +0900

    Update index.py

--- 044/lambda/function1/index.py
+++ 044/lambda/function1/index.py
@@ -27,54 +27,29 @@ def lambda_handler(event, context):
       Bucket=bucket_name,
       Key=key,
       Body=now_str.encode())
-    #print(put_response)
       
     get_response = s3_client.get_object(
-      Bucket=bucket_name,
-      Key=key)
-    #print(get_response)
+      Bucket=bucket_name,　Key=key)
       
     object_ = {
       'Key': key,
       'LastModified': get_response['LastModified'],
       'Size': get_response['ContentLength'],
-      #'StorageClass': get_response['StorageClass'],
       'ETtag': get_response['ETag']
     }
-    #return json.dumps(objects)
     return json.dumps(object_, default=json_serial)
   
   elif event['field'] == LIST:
     list_response = s3_client.list_objects_v2(
       Bucket=bucket_name)
-      
     objects = list_response['Contents']
-    #objects = [
-    #  {
-    #    'key': 'hogehoge.txt',
-    #    'lastModified': '2022-05-04 10:00',
-    #    'size': 100,
-    #    'storageClass': 'STANDARD',
-    #    'etag': '1234567890abc'
-    #  }
-    #]
-    
-    #return json.dumps(objects)
     return json.dumps(objects, default=json_serial)
     
   elif event['field'] == DELETE:
-    #print(event['arguments'])
     key = event['arguments']['Key']
-    
     delete_response = s3_client.delete_object(
-      Bucket=bucket_name,
-      Key=key)
-    #print(delete_response)
-      
+      Bucket=bucket_name,　Key=key)
     object_ = {
       'Key': key
     }
     return json.dumps(object_, default=json_serial)
-    
-  #else:
-  #  return None
