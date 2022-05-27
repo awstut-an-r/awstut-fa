b50a9b29af92c6cf45d14f999ba96adf9e88beba
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-05-27 23:49:50 +0900

    Update index.py

--- 057/lambda/function/index.py
+++ 057/lambda/function/index.py
@@ -5,13 +5,10 @@ import os
 import requests
 from requests.auth import HTTPBasicAuth
 
-#PATH = './bulk_movies.json'
 BULK_ENDPOINT = os.environ['BULK_ENDPOINT']
-#BULK_FILE = os.environ['BULK_FILE']
 BULK_S3_BUCKET = os.environ['BULK_S3_BUCKET']
 BULK_S3_KEY = os.environ['BULK_S3_KEY']
 
-#DOMAIN_ENDPOINT = os.environ['DOMAIN_ENDPOINT']
 MASTER_USERNAME = os.environ['MASTER_USERNAME']
 MASTER_PASSWORD = os.environ['MASTER_PASSWORD']
 
@@ -38,20 +35,6 @@ def lambda_handler(event, context):
         headers={'Content-Type': 'application/json'}
         )
       print(requests_response.text)
-      
-      #with open(BULK_FILE) as f:
-      #  bulk = f.read()
-      #  #print(s)
-      #  #print(requests.get('https://www.google.com/?hl=ja'))
-      #  
-      #  response = requests.post(
-      #    BULK_ENDPOINT,
-      #    #headers={
-      #    #  'Content-Type': 'application/json'
-      #    #},
-      #    json=bulk,
-      #    auth=HTTPBasicAuth(MASTER_USERNAME, MASTER_PASSWORD)
-      #    )
       
     cfnresponse.send(event, context, cfnresponse.SUCCESS, response_data)
         
