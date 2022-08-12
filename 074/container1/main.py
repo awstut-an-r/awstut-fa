3664d122e422658de557ddd6e588d762157cff22
awstut-an-r <84276199+awstut-an-r@users.noreply.github.com>
2022-08-12 22:11:11 +0900

    Update main.py

--- 074/container1/main.py
+++ 074/container1/main.py
@@ -10,16 +10,10 @@ xray_recorder.configure(plugins=plugins)
 
 app.install(XRayMiddleware(xray_recorder))
 
-##xray_recorder.configure(service='fa-074')
-##patch_all()
-
 @app.route('/')
 def hello():
-  ##xray_recorder.begin_segment('fa-074-segment')
-  ##xray_recorder.end_segment()
   return 'Hello X-Ray!'
   
     
 if __name__ == '__main__':
-  #run(app=app, host='0.0.0.0', port=8080)
   run(app=app, host='0.0.0.0', port=8080)
